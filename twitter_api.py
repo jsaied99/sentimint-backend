c77a04ccb355962a2217e0c74af410afa2918c4c
jsaied <jsaied99@gmail.com>
2022-01-19 19:18:52 -0500

    adding average sentiment score, tweet length, and interpretation

--- twitter_api.py
+++ twitter_api.py
@@ -1,14 +1,13 @@
 import requests
+import os
 
-
-bearer_token = "Bearer AAAAAAAAAAAAAAAAAAAAACqUWAEAAAAA81etU%2FOQ0Okmt5N9maHM0%2B62loE%3DnyIIjT6DVpYVhSOAYJqJ8lZNX1i4v8XSzqIa1HQWpRAthgjdKh"
-
+bearer_token = os.environ["TWITTER_API_KEY"]
 
 def get_tweets(topic: str, limit=10) -> list:
     base_url = "https://api.twitter.com/2/tweets/search/recent?query={}&max_results={}".format(
         topic, limit)
-    response = requests.get(base_url, headers={"Authorization": bearer_token})
-
+    
+    response = requests.get(base_url, headers={"Authorization": bearer_token})    
     data = response.json()
     text_array = []
 
